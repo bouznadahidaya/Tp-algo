8356ad1ca19fb016b15c23ace908d04e89d3c64f
bouznadahidaya <bouznadahidaya.didi@gmail.com>
2025-10-19 13:34:29 +0100

    Create Tp 1 exo 1 algo .c

---
+++ Tp 1 exo 1 algo .c
@@ -0,0 +1,22 @@
+
+#include<stdio.h>
+Int main (){
+Int I,j,n, x;
+Printf("donner the size of  matrix=");
+Scanf("%d",n);
+Char mat[n][n];
+For(i=0;i<n;i++){
+For(j=0;j<n;j++){
+Printf ("donner les elemnt  de matrix[%d][%d]= ", i,j);
+scanf ("%d",&mat[i][j]);}}
+for(i=0;i<n;i++){
+for(j=0;j<n;j++){
+x=mat[i][j];
+mat[i][j]=mat[j][i];
+mat[j][i]=x;}}
+for(i=0;i<n;i++){
+for(j=0;j<n;j++){
+printf(" %3d",mat[i][j]);
+printf("\n");
+}}
+return 0;
